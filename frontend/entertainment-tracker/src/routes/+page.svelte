7bef97c8065ad7f9a6d36ba9fb83af0f7a3e5060
Szász Hanna Rebeka <szhannare@mailbox.unideb.hu>
2024-11-07 10:29:31 +0100

    adding/updating placeholder pages

--- frontend/entertainment-tracker/src/routes/+page.svelte
+++ frontend/entertainment-tracker/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Header from './header.svelte';
+</script>
+
+<!--Ezt csak azért raktam ide, hogy át lehessen navigálni a dashboard és addmedia oldalakra vele, nyugodtan vedd ki-->
+<div class="absolute inset-0 bg-background bg-fixed overflow-auto">
+    <Header></Header>
+</div>
---
+++ frontend/entertainment-tracker/src/routes/dashboard/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Header from '../header.svelte';
+</script>
+
+<div  class="absolute inset-0 bg-background bg-fixed overflow-auto">
+    <Header></Header>
+</div>
