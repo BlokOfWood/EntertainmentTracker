3e9598bae59eb7b21eb578918060735df112ba83
Bálint Kozák <kozak.balint15@gmail.com>
2024-11-07 15:28:06 +0100

    Update error handling for duplicate email constraint in UserModel

--- backend/internal/data/users.go
+++ backend/internal/data/users.go
@@ -101,7 +101,7 @@ func (m UserModel) Insert(user *User) error {
 	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt, &user.Version)
 	if err != nil {
 		switch {
-		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+		case err.Error() == "constraint failed: UNIQUE constraint failed: users.email (2067)":
 			return ErrDuplicateEmail
 		default:
 			return err
@@ -196,7 +196,7 @@ func (m UserModel) Update(user *User) error {
 	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Version)
 	if err != nil {
 		switch {
-		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+		case err.Error() == "constraint failed: UNIQUE constraint failed: users.email (2067)":
 			return ErrDuplicateEmail
 		case errors.Is(err, sql.ErrNoRows):
 			return ErrEditConflict
