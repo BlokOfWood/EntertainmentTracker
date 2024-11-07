54dc22d6a08a54a7a3d2aaa91e8dc38f62de90f6
Szász Hanna Rebeka <szhannare@mailbox.unideb.hu>
2024-11-07 14:38:55 +0100

    updating header

--- frontend/entertainment-tracker/src/routes/header.svelte
+++ frontend/entertainment-tracker/src/routes/header.svelte
@@ -3,17 +3,11 @@
     import { onMount } from 'svelte';
 
     const initialUrl = writable(typeof window !== 'undefined' ? window.location.href : '');
-    
-    // Reactive statement
+
     $: currentUrlValue = $initialUrl;
 
-    const isDashboardPage = () => {
-        return currentUrlValue.length > 0 && currentUrlValue.includes('dashboard');
-    };
-
-    const isAddMediaPage = () => {
-        return currentUrlValue.length > 0 && currentUrlValue.includes('addmedia');
-    };
+    const isDashboardPage = () => currentUrlValue.includes('dashboard');
+    const isAddMediaPage = () => currentUrlValue.includes('addmedia');
 
     let showLogout = false;
 
@@ -43,23 +37,22 @@
     }
 </style>
 
-<div class="flex justify-between items-center w-100 bg-header shadow-md pr-3 pl-3">
+<div class="relative flex justify-between items-center w-100 bg-header shadow-md pr-3 pl-3">
     <div class="flex items-center">
-        <div class="border-1 border-solid border-red-600 pl-2 pr-1 w-fit text-white text-right">
+        <div class="pl-2 pr-1 w-fit text-white text-right">
             <img src="/mediamindlogo.png" alt="MediaMind Logo" class="w-6 h-6" />
         </div>
-        <div class="border-1 border-solid border-red-600 pr-8 w-fit text-white text-2xl text-left Ubuntu-font">MediaMind</div>
-        <a href="/dashboard" class="border-1 border-solid border-red-600 p-2 w-fit text-white text-l {isDashboardPage() ? 'font-bold' : ''} Ubuntu-font">Dashboard</a>
-        <a href="/addmedia" class="border-1 border-solid border-red-600 p-2 w-fit text-white text-l {isAddMediaPage() ? 'font-bold' : ''} Ubuntu-font">Add Media</a>
+        <div class="pr-8 w-fit text-white text-2xl text-left Ubuntu-font">MediaMind</div>
+        <a href="/dashboard" class="p-2 w-fit text-white text-l {isDashboardPage() ? 'font-bold' : ''} Ubuntu-font">Dashboard</a>
+        <a href="/addmedia" class="p-2 w-fit text-white text-l {isAddMediaPage() ? 'font-bold' : ''} Ubuntu-font">Add Media</a>
     </div>
     <button class="bg-header text-white p-2 rounded-full flex items-center profile-picture-button" on:click|stopPropagation={toggleLogout} aria-expanded={showLogout}>
         <img src="/profilepicture.png" alt="Profile" class="rounded-full w-10 h-10"/>
     </button>
-</div>
-{#if showLogout}
-<div class="flex justify-end">
-    <a href="/" class="w-fit bg-header pr-3 pl-3 pt-2 pb-2 rounded-br-md rounded-bl-md text-white shadow-md Ubuntu-font">
+    {#if showLogout}
+    <!--TODO: Add logout functionality-->
+    <a href="/" class="absolute right-0 top-full z-50 block w-fit bg-header pr-3 pl-3 pt-2 pb-2 rounded-br-md rounded-bl-md text-white shadow-md Ubuntu-font text-right">
         Logout
     </a>
+    {/if}
 </div>
-{/if}
