da4c376cab11709c03a4701f2a9d9422f5e1cad1
Pap Sándor Dániel <pap.sandor.daniel@gmail.com>
2024-11-09 01:57:59 +0100

    non-functional login and register pages

---
+++ frontend/entertainment-tracker/src/routes/register/+page.svelte
@@ -0,0 +1,32 @@
+
+<div class="absolute left-4 top-4 z-10 flex items-center gap-1 pl-2">
+	<div class="w-fit text-right text-white">
+		<img src="/mediamindlogo.png" alt="MediaMind Logo" class="h-6 w-6" />
+	</div>
+	<div class="Ubuntu-font w-fit pr-8 text-left text-3xl text-white">MediaMind</div>
+</div>
+
+<div
+	class="bg-background Ubuntu-font absolute inset-0 flex items-center justify-center font-[Inter]"
+>
+	<div class="Inter-font w-full max-w-[400px] rounded-2xl bg-white p-6 text-center">
+		<h1 class="mb-8 text-left text-3xl">Register</h1>
+
+		<div>
+			<label class="block text-left" for="email">Email</label>
+			<input class="mt-2 w-full p-1" id="email" type="email" placeholder="Email" />
+		</div>
+
+		<div class="my-4">
+			<label class="block text-left" for="username">Username</label>
+			<input class="mt-2 w-full p-1" id="username" type="text" placeholder="Username" />
+		</div>
+
+		<div class="mt-4 mb-8">
+			<label class="block text-left" for="password">Password</label>
+			<input class="mt-2 w-full p-1" id="password" type="password" placeholder="Password" />
+		</div>
+
+		<button class="bg-background rounded-md px-4 py-2 text-sm text-white">Register</button>
+	</div>
+</div>
