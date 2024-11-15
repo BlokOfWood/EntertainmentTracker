e29ed6521f82b9ad9f920db2900a2b42efb6fc34
Szász Hanna Rebeka <szhannare@mailbox.unideb.hu>
2024-11-15 21:59:53 +0100

    adding media editing with placeholders

--- frontend/entertainment-tracker/src/routes/works/dashboard/+page.svelte
+++ frontend/entertainment-tracker/src/routes/works/dashboard/+page.svelte
@@ -109,6 +109,7 @@
 
 	let checkingDashboard=true;
 	let sharingMedia=false;
+	let editingMedia=false;
 
 	function shareMedia() {
 		checkingDashboard=false;
@@ -123,8 +124,69 @@
 		//TODO: implement the proper function, so it's not just placeholder
 	}
 
-	function editMedia() {
-		//TODO: write edit media function
+	let title="";
+	let type="";
+	let mediaArtSource="/placeholderForEditMediaCoverArt.png";
+	let description="";
+	let categories!: String[];
+	let aimProgress!: number;
+	let author="";
+	let ISBNnumber=""
+	let YTURL="https://www.youtube.com/embed/dQw4w9WgXcQ?si=dourAMMy3-5pBbJr";
+
+	let newProgress!: number;
+	let newVideoTitle="hehe";
+
+	function editMedia(currentTitle: string, currentType: string, goal: number) {
+		checkingDashboard=false;
+		editingMedia=true;
+		title=currentTitle;
+		type=currentType;
+		aimProgress=goal;
+		//TODO: get the proper description for the media
+		description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		//TODO: get the proper categories for the media
+		let newCategories=[
+			"crimi",
+			"fantasy",
+			"horror"
+		]
+		categories=newCategories;
+
+		if(type=="book"){
+			//TODO: get the author properly
+			author="XY";
+			//TODO: get the author properly
+			ISBNnumber="ISBN1234567890";
+		}
+
+		if(type!="YouTubeVideo"){
+			//TODO: get the poster/coverart properly
+			mediaArtSource="/placeholderForEditMediaCoverArt.png";
+		}
+		else {
+			//TODO: get the proper url that's added by the user
+			YTURL="https://youtu.be/dQw4w9WgXcQ?feature=shared"
+
+			//TODO: REMOVE. These are for tests to see if the embedding works for diff vids
+			//YTURL="https://youtu.be/XGNeFrnhlvw?feature=shared"
+			//YTURL="https://youtu.be/Y1ujpoDlgRU?feature=shared"
+
+			//Converts the shared URL to the embed URL~
+			let videoId = YTURL.split("https://youtu.be/")[1].split("?")[0];
+
+			YTURL="https://www.youtube.com/embed/" + videoId + "?si=dourAMMy3-5pBbJr";
+		}
+
+	}
+
+	function mediaEdited(){
+		if(type=="YouTubeVideo"){
+			console.log("New title for video: " + newVideoTitle);
+		}
+		else{
+			console.log("New progress: " + newProgress);
+		}
 	}
 
 	function deleteMedia() {
@@ -134,6 +196,7 @@
 	function returnToDashboard(){
 		checkingDashboard=true;
 		sharingMedia=false;
+		editingMedia=false;
 	}
 </script>
 
@@ -201,7 +264,7 @@
 					<button class="share-button" on:click={shareMedia}>
 						<img src="/share.png" alt="Share" class="w-5 h-5" />
 					</button>
-					<button class="edit-button" on:click={editMedia}>
+					<button class="edit-button" on:click={() => editMedia(work.title, work.type, work.target_progress)}>
 						<img src="/edit.png" alt="Edit" class="w-5 h-5" />
 					</button>
 					<button class="delete-button" on:click={deleteMedia}>
@@ -239,6 +302,88 @@
 				</div>
 			</div>
 		</div>
+	</div>
+</div>
+{/if}
+{#if editingMedia}
+<div class="relative z-0 flex flex-grow items-center justify-center px-80 py-3">
+	<div class="h-full w-full rounded-lg bg-white">
+		<div class="flex flex-col">
+			<div class="flex items-start justify-between p-2">
+				<button class="flex items-center justify-center border-0 pl-6 pt-4" on:click={returnToDashboard}>
+					<img src="/back-button.png" alt="Return to dashboard" class="w-5 h-5"/>
+				</button>
+			</div>
+			<div></div>
+		</div>
+		{#if type!=="YouTubeVideo"}
+		<div class="flex p-4 items-start ">
+			<div class="flex flex-col ml-10 mr-4">
+				<img src={mediaArtSource} alt="Return to dashboard" class="w-full rounded-md"/>
+				{#if type==="book"}
+					<div class="text-center text-xxs Ubuntu-font pt-1">
+						{ISBNnumber}
+					</div>
+				{/if}
+				<div class="text-center text-xxs Ubuntu-font p-1">
+					Categories:
+				</div>
+				{#each categories as category}
+					<div class="text-center text-xxs Ubuntu-font pb-1">
+						{category}
+					</div>
+				{/each}
+			</div>
+			<div class="mr-8 ml-4">
+				<div class="text-center text-sm font-bold Ubuntu-font">
+					{title}
+				</div>
+				{#if type==="book"}
+					<div class="text-center text-xs font-bold Ubuntu-font">
+						{author}
+					</div>
+				{/if}
+				<div class="text-start text-xxs Ubuntu-font pt-3" style="line-height: 2.5;">
+					{description}
+				</div>
+				<div class="Ubuntu-font text-sm font-bold pt-6 pb-2">
+					Progress
+				</div>
+				<div>
+					{#if type==="TVshow"}
+						<input type="progress" placeholder="Episode number / {aimProgress}" bind:value={newProgress} class="border rounded-md p-1.5 mr-1 text-sm" /> 
+					{/if}
+					{#if type==="book"}
+						<input type="progress" placeholder="Page number / {aimProgress}" bind:value={newProgress} class="border rounded-md p-1.5 mr-1 text-sm" /> 
+					{/if}
+					{#if type==="movie"}
+						<input type="progress" placeholder="Minutes / {aimProgress}" bind:value={newProgress} class="border rounded-md p-1.5 mr-1 text-sm" /> 
+					{/if}
+					<button class="bg-background text-white rounded-md py-1.5 px-6 text-sm" on:click={mediaEdited}> 
+						Save
+					</button>
+				</div>
+			</div>
+		</div>
+		{/if}
+		{#if type==="YouTubeVideo"}
+			<div class="flex flex-col mt-6 ml-24 mr-24">
+				<div class="flex justify-center items-center">
+					<iframe title="video" class="w-full aspect-[18/10]"
+						src={YTURL}>
+					</iframe>
+				</div>
+				<div class="Ubuntu-font text-xs pt-6">
+					Set Display name
+				</div>
+				<div class="flex pt-1">
+					<input type="displayTitle" placeholder="Name" bind:value={newVideoTitle} class="border rounded-md p-1.5 mr-1 text-sm w-64"/> 
+					<button class="bg-background text-white rounded-md py-1.5 px-6 text-sm" on:click={mediaEdited}> 
+						Save
+					</button>
+				</div>
+			</div>
+		{/if}
 	</div>
 </div>
 {/if}
