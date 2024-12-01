02714c96cd2adfadf560c301700b8d8ea4d0b644
Bálint Kozák <kozak.balint15@gmail.com>
2024-12-01 01:41:00 +0100

    user endpoint tests

--- backend/cmd/backend/handlers_test.go
+++ backend/cmd/backend/handlers_test.go
@@ -3,155 +3,347 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"io"
-	"log/slog"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/BlokOfWood/EntertainmentTracker/backend/internal/data"
-	"github.com/BlokOfWood/EntertainmentTracker/backend/internal/mocks"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
 )
 
 var mockTime = time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
 
 func TestHealthcheckHandler(t *testing.T) {
-	app := &application{
-		config: config{
-			env: "development",
-			cors: struct {
-				trustedOrigins []string
-			}{
-				trustedOrigins: []string{"http://example.com"},
-			},
-		},
-	}
-	req := httptest.NewRequest(http.MethodGet, "/healthcheck", nil)
-	w := httptest.NewRecorder()
+	app := newTestApplication(t)
 
-	app.healthcheckHandler(w, req)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	res := w.Result()
-	require.Equal(t, http.StatusOK, res.StatusCode)
+	code, _, bodyString := ts.get(t, "/v1/healthcheck")
+	body := map[string]interface{}{}
+	err := json.Unmarshal([]byte(bodyString), &body)
+	assert.NoError(t, err)
 
-	var body map[string]interface{}
-	err := json.NewDecoder(res.Body).Decode(&body)
-	require.NoError(t, err)
+	assert.Equal(t, http.StatusOK, code)
 	assert.Equal(t, "available", body["status"])
-	systemInfo := body["system_info"].(map[string]interface{})
-	assert.Equal(t, "development", systemInfo["environment"])
-	assert.Contains(t, systemInfo["cors_trusted_origins"], "http://example.com")
+	systemInfo, ok := body["system_info"].(map[string]interface{})
+	assert.True(t, ok)
+	assert.Equal(t, "test", systemInfo["environment"])
+	assert.Equal(t, "1.0.0", systemInfo["version"])
+	corsTrustedOrigins, ok := systemInfo["cors_trusted_origins"].([]interface{})
+	assert.True(t, ok)
+	assert.Equal(t, "localhost:6969", corsTrustedOrigins[0])
+	assert.Equal(t, "localhost:8080", corsTrustedOrigins[1])
 }
 
 func TestCreateUserHandler(t *testing.T) {
-	mockUserModel := new(mocks.MockUserModel)
+	app := newTestApplication(t)
 
-	app := &application{
-		models: data.Models{
-			Users: mockUserModel,
-		},
-	}
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
 	input := map[string]string{
 		"name":     "John Doe",
-		"email":    "johndoe@example.com",
+		"email":    "johndoe@gmail.com",
 		"password": "password123",
 	}
-	body, _ := json.Marshal(input)
-
-	req := httptest.NewRequest(http.MethodPost, "/users/register", bytes.NewReader(body))
-	req.Header.Set("Content-Type", "application/json")
-
-	rr := httptest.NewRecorder()
-
-	mockUserModel.On("Insert", mock.MatchedBy(func(user *data.User) bool {
-		okPassword, _ := user.Password.Matches("password123")
-		return user.Name == "John Doe" && user.Email == "johndoe@example.com" && okPassword
-	})).Return(nil)
-
-	app.createUserHandler(rr, req)
-
-	assert.Equal(t, http.StatusCreated, rr.Code)
-
-	var actualResponse map[string]interface{}
-	err := json.Unmarshal(rr.Body.Bytes(), &actualResponse)
+	inputJSON, err := json.Marshal(input)
 	assert.NoError(t, err)
-	user, ok := actualResponse["user"].(map[string]interface{})
+
+	code, _, bodyString := ts.post(t, "/v1/users/register", bytes.NewReader(inputJSON))
+
+	assert.Equal(t, http.StatusCreated, code)
+
+	var body map[string]interface{}
+	err = json.Unmarshal([]byte(bodyString), &body)
+	assert.NoError(t, err)
+	user, ok := body["user"].(map[string]interface{})
 	assert.True(t, ok)
 
-	assert.Equal(t, mockTime.Format(time.RFC3339), user["created_at"])
+	assert.NotEmpty(t, user["created_at"])
 	assert.Equal(t, float64(1), user["id"])
-	assert.Equal(t, "johndoe@example.com", user["email"])
+	assert.Equal(t, "johndoe@gmail.com", user["email"])
 	assert.Equal(t, "John Doe", user["name"])
-
-	mockUserModel.AssertCalled(t, "Insert", mock.MatchedBy(func(user *data.User) bool {
-		return user.Name == "John Doe" && user.Email == "johndoe@example.com"
-	}))
 }
 
 func TestCreateUserHandler_InvalidJSON(t *testing.T) {
-	app := &application{
-		models: data.Models{},
-		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
-	}
+	app := newTestApplication(t)
 
-	req := httptest.NewRequest(http.MethodPost, "/users/register", bytes.NewReader([]byte("{invalid json")))
-	req.Header.Set("Content-Type", "application/json")
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	rr := httptest.NewRecorder()
-	app.createUserHandler(rr, req)
-
-	assert.Equal(t, http.StatusBadRequest, rr.Code)
+	code, _, _ := ts.post(t, "/v1/users/register", bytes.NewReader([]byte("invalid json")))
+	assert.Equal(t, http.StatusBadRequest, code)
 }
 
 func TestCreateUserHandler_ValidationError(t *testing.T) {
-	app := &application{
-		models: data.Models{},
-	}
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
 	input := map[string]string{
 		"name":     "",
 		"email":    "invalidemail",
 		"password": "123",
 	}
-	body, _ := json.Marshal(input)
+	inputJSON, err := json.Marshal(input)
+	assert.NoError(t, err)
 
-	req := httptest.NewRequest(http.MethodPost, "/users", bytes.NewReader(body))
-	req.Header.Set("Content-Type", "application/json")
-
-	rr := httptest.NewRecorder()
-	app.createUserHandler(rr, req)
-
-	assert.Equal(t, http.StatusUnprocessableEntity, rr.Code)
+	code, _, _ := ts.post(t, "/v1/users/register", bytes.NewReader(inputJSON))
+	assert.Equal(t, http.StatusUnprocessableEntity, code)
 }
 
 func TestCreateUserHandler_DuplicateEmail(t *testing.T) {
-	mockUserModel := new(mocks.MockUserModel)
-	mockUserModel.On("Insert", mock.Anything).Return(data.ErrDuplicateEmail)
+	app := newTestApplication(t)
 
-	app := &application{
-		models: data.Models{
-			Users: mockUserModel,
-		},
-	}
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
 	input := map[string]string{
 		"name":     "John Doe",
 		"email":    "johndoe@example.com",
 		"password": "securepassword",
 	}
-	body, _ := json.Marshal(input)
+	inputJSON, err := json.Marshal(input)
+	assert.NoError(t, err)
 
-	req := httptest.NewRequest(http.MethodPost, "/users", bytes.NewReader(body))
-	req.Header.Set("Content-Type", "application/json")
+	code, _, _ := ts.post(t, "/v1/users/register", bytes.NewReader(inputJSON))
+	assert.Equal(t, http.StatusCreated, code)
 
-	rr := httptest.NewRecorder()
-	app.createUserHandler(rr, req)
+	code, _, _ = ts.post(t, "/v1/users/register", bytes.NewReader(inputJSON))
+	assert.Equal(t, http.StatusConflict, code)
+}
 
-	assert.Equal(t, http.StatusConflict, rr.Code)
-	mockUserModel.AssertCalled(t, "Insert", mock.Anything)
+func TestCreateUserHandler_DBError(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	app.db.Close()
+
+	input := map[string]string{
+		"name":     "John Doe",
+		"email":    "john.doe@gmail.com",
+		"password": "password123",
+	}
+
+	inputJSON, err := json.Marshal(input)
+	assert.NoError(t, err)
+
+	code, _, _ := ts.post(t, "/v1/users/register", bytes.NewReader(inputJSON))
+	assert.Equal(t, http.StatusInternalServerError, code)
+}
+
+func TestCreateAuthenticationTokenHandler(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, _ := ts.makeUser(t, &data.User{
+		Email: "john.doe@gmail.com",
+		Name:  "John Doe",
+	}, "password123")
+	assert.Equal(t, http.StatusCreated, code)
+
+	input := map[string]string{
+		"email":    "john.doe@gmail.com",
+		"password": "password123",
+	}
+	inputJSON, err := json.Marshal(input)
+	assert.NoError(t, err)
+
+	code, _, bodyString := ts.post(t, "/v1/users/login", bytes.NewReader(inputJSON))
+	assert.Equal(t, http.StatusCreated, code)
+
+	var body map[string]interface{}
+	err = json.Unmarshal([]byte(bodyString), &body)
+	assert.NoError(t, err)
+	auth, ok := body["authentication_token"].(map[string]interface{})
+	assert.True(t, ok)
+
+	assert.NotEmpty(t, auth["token"])
+	assert.NotEmpty(t, auth["expiry"])
+}
+
+func TestCreateAuthenticationTokenHandler_InvalidJSON(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, _ := ts.post(t, "/v1/users/login", bytes.NewReader([]byte("invalid json")))
+	assert.Equal(t, http.StatusBadRequest, code)
+}
+
+func TestCreateAuthenticationTokenHandler_ValidationError(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	input := map[string]string{
+		"email":    "invalidemail",
+		"password": "123",
+	}
+	inputJSON, err := json.Marshal(input)
+	assert.NoError(t, err)
+
+	code, _, _ := ts.post(t, "/v1/users/login", bytes.NewReader(inputJSON))
+	assert.Equal(t, http.StatusUnprocessableEntity, code)
+}
+
+func TestCreateAuthenticationTokenHandler_UserNotFound(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	input := map[string]string{
+		"email":    "joe.biden@gmail.com",
+		"password": "password123",
+	}
+	inputJSON, err := json.Marshal(input)
+	assert.NoError(t, err)
+
+	code, _, _ := ts.post(t, "/v1/users/login", bytes.NewReader(inputJSON))
+	assert.Equal(t, http.StatusUnauthorized, code)
+}
+
+func TestCreateAuthenticationTokenHandler_IncorrectPassword(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, _ := ts.makeUser(t, nil, "")
+	assert.Equal(t, http.StatusCreated, code)
+
+	input := map[string]string{
+		"email":    "john.doe@gmail.com",
+		"password": "incorrectpassword",
+	}
+	inputJSON, err := json.Marshal(input)
+	assert.NoError(t, err)
+
+	code, _, _ = ts.post(t, "/v1/users/login", bytes.NewReader(inputJSON))
+	assert.Equal(t, http.StatusUnauthorized, code)
+}
+
+func TestCreateAuthenticationTokenHandler_DBError(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	app.db.Close()
+
+	input := map[string]string{
+		"email":    "john.doe@gmail.com",
+		"password": "password123",
+	}
+	inputJSON, err := json.Marshal(input)
+	assert.NoError(t, err)
+
+	code, _, _ := ts.post(t, "/v1/users/login", bytes.NewReader(inputJSON))
+	assert.Equal(t, http.StatusInternalServerError, code)
+}
+
+func TestGetUserHandler(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, _ := ts.makeUser(t, nil, "")
+	assert.Equal(t, http.StatusCreated, code)
+
+	token := ts.getToken(t, "", "")
+
+	code, _, bodyString := ts.getWithAuth(t, "/v1/users/me", token)
+
+	var body map[string]interface{}
+	err := json.Unmarshal([]byte(bodyString), &body)
+	assert.NoError(t, err)
+	user, ok := body["user"].(map[string]interface{})
+	assert.True(t, ok)
+
+	assert.Equal(t, "John Doe", user["name"])
+	assert.Equal(t, "john.doe@gmail.com", user["email"])
+	assert.Equal(t, float64(1), user["id"])
+	assert.NotEmpty(t, user["created_at"])
+}
+
+func TestGetUserHandler_Unauthenticated(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, _ := ts.get(t, "/v1/users/me")
+	assert.Equal(t, http.StatusUnauthorized, code)
+}
+
+func TestGetUserHandler_DBError(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, _ := ts.makeUser(t, nil, "")
+	assert.Equal(t, http.StatusCreated, code)
+
+	token := ts.getToken(t, "", "")
+
+	app.db.Close()
+
+	code, _, _ = ts.getWithAuth(t, "/v1/users/me", token)
+	assert.Equal(t, http.StatusInternalServerError, code)
+}
+
+func TestInvalidateAuthenticationTokenHandler(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, _ := ts.makeUser(t, nil, "")
+	assert.Equal(t, http.StatusCreated, code)
+
+	token := ts.getToken(t, "", "")
+
+	code, _, _ = ts.getWithAuth(t, "/v1/users/logout", token)
+	assert.Equal(t, http.StatusOK, code)
+
+	code, _, _ = ts.getWithAuth(t, "/v1/users/me", token)
+	assert.Equal(t, http.StatusUnauthorized, code)
+}
+
+func TestInvalidateAuthenticationTokenHandler_Unauthenticated(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, _ := ts.get(t, "/v1/users/logout")
+	assert.Equal(t, http.StatusUnauthorized, code)
+}
+
+func TestInvalidateAuthenticationTokenHandler_DBError(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, _ := ts.makeUser(t, nil, "")
+	assert.Equal(t, http.StatusCreated, code)
+
+	token := ts.getToken(t, "", "")
+
+	app.db.Close()
+
+	code, _, _ = ts.getWithAuth(t, "/v1/users/logout", token)
+	assert.Equal(t, http.StatusInternalServerError, code)
 }
