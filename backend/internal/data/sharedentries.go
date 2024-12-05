b90ca376922d0d4dbe340568d8e9b40db73d2834
Bálint Kozák <kozak.balint15@gmail.com>
2024-12-05 12:19:44 +0100

    Refactor shared entries database operations to use context and parameterized queries; Fix delete query

--- backend/internal/data/sharedentries.go
+++ backend/internal/data/sharedentries.go
@@ -1,7 +1,10 @@
 package data
 
 import (
+	"context"
 	"database/sql"
+	"errors"
+	"time"
 
 	"github.com/BlokOfWood/EntertainmentTracker/backend/internal/validator"
 )
@@ -32,16 +35,29 @@ func ValidateIds(v *validator.Validator, sharedBy, sharedWith int64) {
 }
 
 func (m *SharedEntryModelDB) Insert(entryID, sharedBy, sharedWith int64) error {
-	stmt := `INSERT INTO shared_entries (entry_id, shared_by, shared_with) VALUES (?, ?, ?)`
-	_, err := m.DB.Exec(stmt, entryID, sharedBy, sharedWith)
-	return err
+	stmt := `INSERT INTO shared_entries (entry_id, shared_by, shared_with) VALUES ($1, $2, $3) RETURNING id, created_at`
+	args := []interface{}{entryID, sharedBy, sharedWith}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.QueryContext(ctx, stmt, args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (m *SharedEntryModelDB) Get(id int64) (*SharedEntry, error) {
 	stmt := `SELECT id, entry_id, shared_by, shared_with, created_at
-		FROM shared_entries WHERE id = ?`
+		FROM shared_entries WHERE id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	sharedEntry := &SharedEntry{}
-	err := m.DB.QueryRow(stmt, id).Scan(&sharedEntry.ID, &sharedEntry.EntryID, &sharedEntry.SharedBy, &sharedEntry.SharedWith, &sharedEntry.CreatedAt)
+	err := m.DB.QueryRowContext(ctx, stmt, id).Scan(&sharedEntry.ID, &sharedEntry.EntryID, &sharedEntry.SharedBy, &sharedEntry.SharedWith, &sharedEntry.CreatedAt)
 	if err != nil {
 		return nil, err
 	}
@@ -50,59 +66,111 @@ func (m *SharedEntryModelDB) Get(id int64) (*SharedEntry, error) {
 
 func (m *SharedEntryModelDB) GetBySharedWithID(sharedWith int64) ([]*SharedEntry, error) {
 	stmt := `SELECT id, entry_id, shared_by, shared_with, created_at
-		FROM shared_entries WHERE shared_with = ?`
-	rows, err := m.DB.Query(stmt, sharedWith)
+		FROM shared_entries WHERE shared_with = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, stmt, sharedWith)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
+
 	sharedEntries := []*SharedEntry{}
+
 	for rows.Next() {
 		sharedEntry := &SharedEntry{}
-		err := rows.Scan(&sharedEntry.ID, &sharedEntry.EntryID, &sharedEntry.SharedBy, &sharedEntry.SharedWith, &sharedEntry.CreatedAt)
+		err := rows.Scan(
+			&sharedEntry.ID,
+			&sharedEntry.EntryID,
+			&sharedEntry.SharedBy,
+			&sharedEntry.SharedWith,
+			&sharedEntry.CreatedAt,
+		)
 		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				return nil, ErrRecordNotFound
+			}
 			return nil, err
 		}
 		sharedEntries = append(sharedEntries, sharedEntry)
 	}
+
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
+
 	return sharedEntries, nil
 }
 
 func (m *SharedEntryModelDB) GetAll() ([]*SharedEntry, error) {
 	stmt := `SELECT id, entry_id, shared_by, shared_with, created_at
 		FROM shared_entries`
-	rows, err := m.DB.Query(stmt)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, stmt)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
+
 	sharedEntries := []*SharedEntry{}
+
 	for rows.Next() {
 		sharedEntry := &SharedEntry{}
-		err := rows.Scan(&sharedEntry.ID, &sharedEntry.EntryID, &sharedEntry.SharedBy, &sharedEntry.SharedWith, &sharedEntry.CreatedAt)
+		err := rows.Scan(
+			&sharedEntry.ID,
+			&sharedEntry.EntryID,
+			&sharedEntry.SharedBy,
+			&sharedEntry.SharedWith,
+			&sharedEntry.CreatedAt,
+		)
 		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				return nil, ErrRecordNotFound
+			}
 			return nil, err
 		}
 		sharedEntries = append(sharedEntries, sharedEntry)
 	}
+
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
+
 	return sharedEntries, nil
 }
 
 func (m *SharedEntryModelDB) Update(sharedEntry *SharedEntry) error {
-	stmt := `UPDATE shared_entries SET entry_id = ?, shared_by = ?, shared_with = ?
-		WHERE id = ?`
-	_, err := m.DB.Exec(stmt, sharedEntry.EntryID, sharedEntry.SharedBy, sharedEntry.SharedWith, sharedEntry.ID)
-	return err
+	stmt := `UPDATE shared_entries SET entry_id = $1, shared_by = $2, shared_with = $3
+		WHERE id = $4`
+	args := []interface{}{sharedEntry.EntryID, sharedEntry.SharedBy, sharedEntry.SharedWith, sharedEntry.ID}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.QueryContext(ctx, stmt, args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (m *SharedEntryModelDB) Delete(id, userID int64) error {
-	stmt := `DELETE FROM shared_entries WHERE id = ? and shared_with = ?`
-	_, err := m.DB.Exec(stmt, id, userID)
-	return err
+	stmt := `DELETE FROM shared_entries WHERE entry_id = $1 and shared_with = $2`
+	args := []interface{}{id, userID}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.QueryContext(ctx, stmt, args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
